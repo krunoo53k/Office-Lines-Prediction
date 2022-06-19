edfd2bb7aedd854368fbfbfb61838bf564327c8e
krunoo53k <petrik.kpetrik@gmail.com>
2022-06-20 01:39:09 +0200

    Adding user input

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import spacy
 
 nlp=spacy.load("en_office_prediction")
 nlp2=spacy.load("en_office_prediction-0.0.2")
-doc=nlp("That's what she said!")
-doc2=nlp2("That's what she said!")
+quote=input("Enter a quote from the Office: ")
+doc=nlp(quote)
+doc2=nlp2(quote)
 
 print(doc.cats)
 print(doc2.cats)
